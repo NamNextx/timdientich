2d695ec7bbdc96067824fe805c0178176aa71bfd
Namnextx <phuongnam.nextx@gmail.com>
2019-01-10 10:39:11 +0700

    done

--- filefunction.js
+++ filefunction.js
@@ -1,6 +1,6 @@
 function timdt() {
     var width =parseInt(prompt("nhap vao gia tri cua width"));
-    var hieght = parseInt(prompt("nhap vao gia tri hieght"));
+    var hieght = parseInt(prompt("nhap vao gia tri hieght a"));
     var are=width*hieght;
     document.write("Enter width: " + width+"</br>"+"Enter Hieght: "+ hieght+"</br>"+"The are is: " + are);
 }
