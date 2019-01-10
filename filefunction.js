2b0b35beeb4ab05889ba292bb15cb931118937d8
Namnextx <phuongnam.nextx@gmail.com>
2019-01-10 10:22:23 +0700

    done

---
+++ filefunction.js
@@ -0,0 +1,6 @@
+function timdt() {
+    var width =parseInt(prompt("nhap vao gia tri cua width"));
+    var hieght = parseInt(prompt("nhap vao gia tri hieght"));
+    var are=width*hieght;
+    alert(are);
+}
